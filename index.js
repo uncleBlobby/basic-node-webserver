6d3b03288d8a884758bbff968634ffd1162b9070
Dustin <dc.christianson@gmail.com>
2021-12-11 10:00:26 -0600

    added basic url validation

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const fs = require('fs')
 const http = require('http')
 
 const port = process.env.PORT || 3000
+const pages = ["/404.html", "/about.html", "/contact-me.html", "/index.html"];
 
 const server = http.createServer((req, res) => {
     // redirect main URL to index page
@@ -12,6 +13,10 @@ const server = http.createServer((req, res) => {
     // append html tag if missing from request
     if (req.url.substring(req.url.length - 5) != ".html"){
         req.url += ".html";
+    }
+    if (!validateURL(req.url, pages))
+    {
+        req.url = "/404.html";
     }
     // read html file and dispatch to browser
     fs.readFile(__dirname + req.url, function(err, data) {
@@ -30,3 +35,16 @@ const server = http.createServer((req, res) => {
 server.listen(port, () => {
     console.log(`Server running at port ${port}`)
 })
+
+function validateURL(url, pages)
+{
+    for(let i = 0; i < pages.length; i++)
+    {
+        if(url == pages[i])
+        {
+            return true;
+        }
+    }
+
+    return false;
+}
